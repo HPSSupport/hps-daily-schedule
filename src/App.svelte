ce5725ed89691d164a61fe4d5d482e5014787e7d
HPSSupport <support@hpsvikings.org>
2020-11-10 10:25:35 -0500

    Get schedule from my json server
    Created ScheduleStore and adjusted logic to create schedule components.
    
    Bugfix: 12 o'clock times now disply PM instead of AM

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script>
+  import axios from 'axios';
   import { time } from './stores/TimeStore.js';
+  import { sched } from './stores/SchechduleStore.js';
   import Schedule from './components/Schedule.svelte';
 
   const formatter = new Intl.DateTimeFormat('en', {
@@ -12,24 +14,6 @@
   const isABDay = () => {
     return;
   };
-
-  const SCHED_HHSA = [
-    { id: '1', start: '7:45', end: '8:50', nextStart: '9:00' },
-    { id: '2', start: '9:00', end: '10:05', nextStart: '10:10' },
-    { id: 'L', start: '10:10', end: '10:40', nextStart: '10:45' },
-    { id: '3', start: '10:45', end: '12:13', nextStart: '12:23' },
-    { id: '4', start: '12:23', end: '13:28', nextStart: '13:38' },
-    { id: '5', start: '13:38', end: '14:43', nextStart: '14:43' },
-  ];
-
-  const SCHED_HHSB = [
-    { id: '1', start: '7:45', end: '8:50', nextStart: '9:00' },
-    { id: '2', start: '9:00', end: '10:05', nextStart: '10:15' },
-    { id: '3', start: '10:15', end: '11:43', nextStart: '11:48' },
-    { id: 'L', start: '11:48', end: '12:18', nextStart: '12:23' },
-    { id: '4', start: '12:23', end: '13:28', nextStart: '13:38' },
-    { id: '5', start: '13:38', end: '14:43', nextStart: '14:43' },
-  ];
 </script>
 
 <style>
@@ -47,10 +31,12 @@
 
   <div class="">
     <div class="row">
-      <div class="col s12">
-        <Schedule sched={SCHED_HHSA} title="HHS A-Lunch" />
-        <Schedule sched={SCHED_HHSB} title="HHS B-Lunch" />
-      </div>
+      {#if $sched.length > 0}
+        <div class="col s12">
+          <Schedule sched={$sched[0].periods} title={$sched[0].title} />
+          <Schedule sched={$sched[1].periods} title={$sched[1].title} />
+        </div>
+      {/if}
     </div>
   </div>
 </main>
