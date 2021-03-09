03768b1f8272602b99a4e52e64674b8b50ab0516
Derrick Price <44605379+HPSSupport@users.noreply.github.com>
2021-03-09 15:11:44 -0500

    Revert "Test row"
    This reverts commit 2476f0be5adbc513fee0d91df123159bca6c6a9f.

--- src/App.svelte
+++ src/App.svelte
@@ -34,7 +34,7 @@
   </nav>
 
   <div class="">
-    <div class="row">
+    <div class="row mb-0">
       {#if $hhsSched.length > 0}
         <div class="col s10">
           <Schedule sched={$hhsSched[0].periods} title={$hhsSched[0].title} />
